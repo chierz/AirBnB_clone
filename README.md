caaebec703690ee7c016d619ef7e895902f67bcb
chierz <bonanwaeze@gmail.com>
2024-05-18 14:09:14 -0700

    Authors and readme of the file

---
+++ README.md
@@ -0,0 +1,1 @@
+Alx Airnbnb Clone
--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,11 @@ class BaseModel:
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(val, tform)
                 else:
-                    self.__dict__[k] = v
+                    self.__dict__[key] = val
         else:
             models.storage.new(self)
 
